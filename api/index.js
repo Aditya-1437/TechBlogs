6d5901e8f2c61743a9a608e693636f7b4b69c76d
Aditya.K <kunchaaditya369@gmail.com>
2024-06-24 17:56:47 +0530

    Error handler created

--- api/index.js
+++ api/index.js
@@ -23,3 +23,12 @@ app.listen(3000, ()=>{
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Maybe error on our side :(';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
