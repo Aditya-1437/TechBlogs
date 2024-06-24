a37f3c2868396534fdb456996d5e6e7985046b09
Aditya.K <kunchaaditya369@gmail.com>
2024-06-24 21:36:22 +0530

    added functionality to signUp page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,46 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, json, useNavigate } from 'react-router-dom'
 
 export default function Signup() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handelChange = (e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+  
+  const handelSubmit = async(e)=>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage("Dragons leave nothing. Fill everything");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/sign-up',{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success==false){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -20,25 +58,34 @@ export default function Signup() {
         </div>
         {/* right */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handelSubmit}>
             <div>
               <Label value='DragonName' />
-              <TextInput type='text' placeholder='Alphabets only' id='username' />
+              <TextInput type='text' placeholder='Alphabets only' id='username' onChange={handelChange} />
             </div>
             <div>
               <Label value='E-Mail' />
-              <TextInput type='email' placeholder='dragon@fire.com' id='email' />
+              <TextInput type='email' placeholder='dragon@fire.com' id='email' onChange={handelChange} />
             </div>
             <div>
               <Label value='Password' />
-              <TextInput type='password' placeholder='dragons posses strong senses' id='password' />
+              <TextInput type='password' placeholder='dragons posses strong senses' id='password' onChange={handelChange} />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>Become the Dragon</Button>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {loading ? (<><Spinner size='sm' /><span className='pl-3'>Loading...</span></>) : 'Become the Dragon'}
+            </Button>
           </form>
           <div className='flex gap-2 text-sm mt-3'>
             <span>Already a dragon?</span>
             <Link to='/sign-in' className='text-purple-500'>Enter the Dungeon</Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
